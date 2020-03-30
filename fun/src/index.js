256b5afcf1beaa40e335382c1b307d9b735e3475
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 11:38:12 -0400

    Props as text, numbers style, jsx expressions for numbers

--- fun/src/index.js
+++ fun/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// JSX as class
 class Message extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Hello Everyone</h1>
+        {/* guess this is decent, have like nothing to define */}
+				<h1 style={{color: this.props.color}}>
+					{this.props.msg}
+				</h1>
+				<p>I'll check back in {this.props.minutes} minutes</p>
 			</div>
 		)
 	}
 }
 
-ReactDOM.render(<Message />, document.getElementById('root'))
+ReactDOM.render(
+  // basic props
+	<Message color="blue" msg="Test" minutes={100}/>, 
+	document.getElementById('root')
+)
