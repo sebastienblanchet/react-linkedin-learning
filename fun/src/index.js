3738bcc526680a177b0a513a30d2dc4f9e9d4789
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 12:09:21 -0400

    Child components, using objs

--- fun/src/index.js
+++ fun/src/index.js
@@ -1,27 +1,85 @@
 import React from 'react'
-import { render } from 'react-dom'
+import {
+	render
+} from 'react-dom'
 
-const Book = ({title, author, pages}) => {
-	return (
-		<section>
-			<h2>{title}</h2>
-			<p>by: {author}</p>
-			<p>Pages: {pages} pages</p>
-		</section>
+let bookList = [{
+		"title": "Hunger",
+		"author": "Roxane Gay",
+		"pages": 320
+	},
+	{
+		"title": "The Sun Also Rises",
+		"author": "Ernest Hemingway",
+		"pages": 260
+	},
+	{
+		"title": "White Teeth",
+		"author": "Zadie Smith",
+		"pages": 480
+	},
+	{
+		"title": "Cat's Cradle",
+		"author": "Kurt Vonnegut",
+		"pages": 304
+	}
+]
+
+const Book = ({
+	title,
+	author,
+	pages
+}) => {
+	return ( <
+		section >
+		<
+		h2 > {
+			title
+		} < /h2> <
+		p > by: {
+			author
+		} < /p> <
+		p > Pages: {
+			pages
+		}
+		pages < /p> <
+		/section>
 	)
 }
 
-const Library = () => {
-	return (
-		<div>
-			<Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260}/>
-			<Book title="White Teeth" author="Zadie Smith" pages={480}/>
-			<Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304}/>
-		</div>
+const Library = ({
+	books
+}) => {
+	return ( <
+		div > {
+			books.map(
+				(book, i) =>
+				<
+				Book key = {
+					i
+				}
+				title = {
+					book.title
+				}
+				author = {
+					book.author
+				}
+				pages = {
+					book.pages
+				}
+				/>
+			)
+		} <
+		/div>
 	)
 }
 
-render(
-	<Library />,
+
+
+render( <
+	Library books = {
+		bookList
+	}
+	/>,
 	document.getElementById('root')
 )
