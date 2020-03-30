a0c2fcef6ba936939be34c2f44cb325a681bc715
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 12:40:24 -0400

    Deal the Zuck's accesibility warnings

--- fun/src/index.js
+++ fun/src/index.js
@@ -40,11 +40,8 @@ class Library extends React.Component {
   }
 
   componentDidMount() {
-    // this is usually where youg et the init fetch
-    // common
-    // fetch as get
     this.setState({ loading: true })
-    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/5')
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
       .then(data => data.json())
       .then(data => this.setState({ data, loading: false }))
   }
@@ -63,22 +60,20 @@ class Library extends React.Component {
     return (
       <div>
         {this.state.hiring ? <Hiring /> : <NotHiring />}
-        {/* cond formatting based on fetch */}
         {this.state.loading
           ? "loading..."
           : <div>
-            {/* use the .map run this create fxn for each product */}
-            {/* react is gonna bitch that it needs a key use that later */}
             {this.state.data.map(product => {
               return (
-                <div>
+                // heres the key
+                <div key={product.id}>
                   <h3>Library Product of the Week!</h3>
                   <h4>{product.name}</h4>
-                  <img src={product.image} height={100} />
+                  {/* resolve some bs mf ass warnings*/}
+                  <img alt={product.name} src={product.image} height={100} />
                 </div>
               )
             })}
-
           </div>
         }
         <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
