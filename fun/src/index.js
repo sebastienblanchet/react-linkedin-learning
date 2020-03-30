e4751505b9f8bf02a4ae663e7e0c53a567e75a98
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 12:17:10 -0400

    Intro to state, constructor

--- fun/src/index.js
+++ fun/src/index.js
@@ -1,85 +1,55 @@
 import React from 'react'
-import {
-	render
-} from 'react-dom'
+import { render } from 'react-dom'
 
-let bookList = [{
-		"title": "Hunger",
-		"author": "Roxane Gay",
-		"pages": 320
-	},
-	{
-		"title": "The Sun Also Rises",
-		"author": "Ernest Hemingway",
-		"pages": 260
-	},
-	{
-		"title": "White Teeth",
-		"author": "Zadie Smith",
-		"pages": 480
-	},
-	{
-		"title": "Cat's Cradle",
-		"author": "Kurt Vonnegut",
-		"pages": 304
-	}
+let bookList = [
+	{ "title": "Hunger", "author": "Roxane Gay", "pages": 320 },
+	{ "title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
+	{ "title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
+	{ "title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 }
 ]
 
-const Book = ({
-	title,
-	author,
-	pages
-}) => {
-	return ( <
-		section >
-		<
-		h2 > {
-			title
-		} < /h2> <
-		p > by: {
-			author
-		} < /p> <
-		p > Pages: {
-			pages
+const Book = ({ title, author, pages }) => {
+	return (
+		<section>
+			<h2>{title}</h2>
+			<p>by: {author}</p>
+			<p>Pages: {pages} pages</p>
+		</section>
+	)
+}
+
+// es6 classes
+class Library extends React.Component {
+	// this is what allows us to keep a state
+	constructor(props) {
+		super(props)
+		this.state = {
+			open: true
 		}
-		pages < /p> <
-		/section>
-	)
-}
-
-const Library = ({
-	books
-}) => {
-	return ( <
-		div > {
-			books.map(
-				(book, i) =>
-				<
-				Book key = {
-					i
-				}
-				title = {
-					book.title
-				}
-				author = {
-					book.author
-				}
-				pages = {
-					book.pages
-				}
-				/>
-			)
-		} <
-		/div>
-	)
-}
-
-
-
-render( <
-	Library books = {
-		bookList
 	}
-	/>,
+	render() {
+		//  always destructure
+		const { books } = this.props
+		return (
+			<div>
+				{/* conditional rendering... */}
+				<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+				{books.map(
+					(book, i) =>
+						<Book
+							key={i}
+							title={book.title}
+							author={book.author}
+							pages={book.pages} />
+				)}
+			</div>
+		)
+	}
+}
+
+
+
+render(
+	<Library books={bookList} />,
 	document.getElementById('root')
 )
