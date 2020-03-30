1c307a14fcba0823a49271cde14d358d5c5b6a46
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 12:28:42 -0400

    State as props

--- fun/src/index.js
+++ fun/src/index.js
@@ -2,56 +2,59 @@ import React from 'react'
 import { render } from 'react-dom'
 
 let bookList = [
-	{ "title": "Hunger", "author": "Roxane Gay", "pages": 320 },
-	{ "title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
-	{ "title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
-	{ "title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 }
+  { "title": "Hunger", "author": "Roxane Gay", "pages": 320 },
+  { "title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
+  { "title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
+  { "title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 }
 ]
 
-const Book = ({ title, author, pages }) => {
-	return (
-		<section>
-			<h2>{title}</h2>
-			<p>by: {author}</p>
-			<p>Pages: {pages} pages</p>
-		</section>
-	)
+const Book = ({ title, author, pages, freeBookmark }) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages} pages</p>
+      {/*  defn of cond format */}
+      <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
+    </section>
+  )
 }
 
 class Library extends React.Component {
-	// make this as static var
-	// allows us to get rid of ctor
-	state = { open: false }
+  // pass state down to chil
+  state = {
+    open: true,
+    freeBookmark: false
+  }
 
-	// arrow funcs alwasy bind this
-	toggleOpenClosed = () => {
-		this.setState(prevState => ({
-			open: !prevState.open
-		}))
-	}
-	render() {
-		const { books } = this.props
-		return (
-			<div>
-				<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
-				{/* connection to sate */}
-				<button onClick={this.toggleOpenClosed}>Change</button>
-				{books.map(
-					(book, i) =>
-						<Book
-							key={i}
-							title={book.title}
-							author={book.author}
-							pages={book.pages} />
-				)}
-			</div>
-		)
-	}
+  toggleOpenClosed = () => {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }))
+  }
+  render() {
+    const { books } = this.props
+    return (
+      <div>
+        <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+        <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map(
+          (book, i) =>
+            <Book
+              key={i}
+              title={book.title}
+              author={book.author}
+              pages={book.pages}
+              freeBookmark={this.state.freeBookmark} />
+        )}
+      </div>
+    )
+  }
 }
 
 
 
 render(
-	<Library books={bookList} />,
-	document.getElementById('root')
+  <Library books={bookList} />,
+  document.getElementById('root')
 )
