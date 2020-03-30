cc8994770d8d7758e69bf8cdbafb354aaef8e6a7
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 11:31:46 -0400

    Using jsx, ugly

--- fun/src/index.js
+++ fun/src/index.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-var style ={
-  backgroundColor: 'black',
-  color: 'white'
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
 }
 
-const title = React.createElement(
-  "h1",
-  {id: "title", className: "header", style:style},
-  "Test"
+ReactDOM.render(
+    <div>
+      <h1>Test</h1>
+      <p>Text</p>
+    </div>,
+    document.getElementById('root')
 )
-
-ReactDOM.render(title, document.getElementById('root'));
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
