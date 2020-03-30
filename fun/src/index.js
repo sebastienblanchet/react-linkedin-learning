cc4ce7d0740414f621776739c67b2f86e957005d
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 12:06:03 -0400

    Multiple components, lib example

--- fun/src/index.js
+++ fun/src/index.js
@@ -1,45 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
+const Book = ({title, author, pages}) => {
+	return (
+		<section>
+			<h2>{title}</h2>
+			<p>by: {author}</p>
+			<p>Pages: {pages} pages</p>
+		</section>
+	)
 }
 
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total/goal)
-}
-
-// destructuring the props
-const SkiDayCounter = ({total, powder, backcountry, goal}) => {
-  return (
-    <section>
-        <div>
-          <p>Total Days: {total}</p>
-        </div>
-        <div>
-          <p>Powder Days: {powder}</p>
-        </div>
-        <div>
-          <p>Backcountry Days: {backcountry}</p>
-        </div>
-        <div>
-          <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-        </div>
-    </section>
-  )
+const Library = () => {
+	return (
+		<div>
+			<Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260}/>
+			<Book title="White Teeth" author="Zadie Smith" pages={480}/>
+			<Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304}/>
+		</div>
+	)
 }
 
 render(
-  <SkiDayCounter 
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal}/>, 
-  document.getElementById('root')
+	<Library />,
+	document.getElementById('root')
 )
