615774a63f7888fe78a23bef13097156d11bcf5a
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 11:28:59 -0400

    Simple create react element

--- fun/src/index.js
+++ fun/src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var style ={
+  backgroundColor: 'black',
+  color: 'white'
+}
+
+const title = React.createElement(
+  "h1",
+  {id: "title", className: "header", style:style},
+  "Test"
+)
+
+ReactDOM.render(title, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
