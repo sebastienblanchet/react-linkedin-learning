89b7cbdef247d9bd361515a6ed8155a2de3b9779
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 12:03:58 -0400

    Move functions as custom methods

--- fun/src/index.js
+++ fun/src/index.js
@@ -2,46 +2,44 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 let skiData = {
-	total: 50,
-	powder: 20,
-	backcountry: 10,
-	goal: 100
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
 }
 
-class SkiDayCounter extends Component {
-  // simply add custom methods
-	getPercent = decimal => {
-		return decimal * 100 + '%'
-	}
-	calcGoalProgress = (total, goal) => {
-		return this.getPercent(total/goal)
-	}
-	render() {
-		const {total, powder, backcountry, goal} = this.props
-		return (
-			<section>
-				<div>
-					<p>Total Days: {total}</p>
-				</div>
-				<div>
-					<p>Powder Days: {powder}</p>
-				</div>
-				<div>
-					<p>Backcountry Days: {backcountry}</p>
-				</div>
-				<div>
-					<p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
-				</div>
-			</section>
-		)
-	}
+const getPercent = decimal => {
+  return decimal * 100 + '%'
+}
+const calcGoalProgress = (total, goal) => {
+  return getPercent(total/goal)
+}
+
+// destructuring the props
+const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+  return (
+    <section>
+        <div>
+          <p>Total Days: {total}</p>
+        </div>
+        <div>
+          <p>Powder Days: {powder}</p>
+        </div>
+        <div>
+          <p>Backcountry Days: {backcountry}</p>
+        </div>
+        <div>
+          <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+        </div>
+    </section>
+  )
 }
 
 render(
-	<SkiDayCounter 
-		total={skiData.total}
-		powder={skiData.powder}
-		backcountry={skiData.backcountry}
-		goal={skiData.goal}/>, 
-	document.getElementById('root')
+  <SkiDayCounter 
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.goal}/>, 
+  document.getElementById('root')
 )
